ea78572d8ea839ce714c4de6e79f61ba2c8cf82c
Wesley Simões <wesleyjipa@hotmail.com>
2022-06-04 11:12:18 -0300

    feat: list all todos by user (front-end)

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -5,14 +5,76 @@
                 Lista de tarefas
             </div>
         </div>
+
+        <form
+            class="flex items-center px-4 bg-gray-300 h-15 rounded-sm mb-3"
+            @submit.stop.prevent="createTodo"
+        >
+            <input
+                v-model="newTodo"
+                placeholder="Digite o nome da sua lista ..."
+                type="text"
+                class="bg-gray-300 placeholder-gray-500 text-gray-700 font-light focus:outline-none block w-full appearance-none leading-normal pr-3"
+            >
+
+            <button
+                type="submit"
+                class="text-blue-700 text-xs font-semibold focus:outline-none"
+            >
+                ADICIONAR
+            </button>
+        </form>
+
+        <div
+            v-if="spinner.get_todos"
+            class="text-center"
+        >
+            <img
+                src="@/assets/img/spinner.svg"
+                alt=""
+                class="inline-block w-5 h-5"
+            >
+        </div>
+
+        <div
+            v-for="todo in todos"
+            :key="todo.id"
+            class="flex items-center justify-btweeen bg-gray-300 rounded-sm px-4 h-15 mb-2"
+        >
+            <div>
+                {{ todo.label }}
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 
 export default {
-  name: 'Home',
-  components: {
-  }
+    name: 'Home',
+
+    data() {
+        return {
+            todos: '',
+            spinner: {
+                get_todos: false,
+            },
+        };
+    },
+
+    created() {
+        this.getTodos();
+    },
+
+    methods: {
+        getTodos() {
+            this.spinner.get_todos = true;
+            this.$axios.get('v1/todos').then((response) => {
+                this.todos = response.data.data;
+            }).finally(() => {
+                this.spinner.get_todos = false;
+            })
+        }
+    },
 }
 </script>
